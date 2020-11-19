1cc748126859867055c2a48cb76853cc93e32557
Hare <anna.ahiker1822@gmail.com>
2020-11-20 01:11:01 +0800

    .

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     transpileDependencies: ['vuetify'],
-    publicPath: process.env.NODE_ENV === 'production' ? 'public' : '',
+    publicPath: '',
     outputDir: '../server/public',
     devServer: {
         proxy: 'http://localhost:3000'
--- server/index.php
+++ server/index.php
@@ -3,9 +3,9 @@ $args = explode('/', $_SERVER['REQUEST_URI']);
 
 print_r($args);
 
-// if ($args[1] == '') {
-//     redirect('public');
-// }
+if ($args[1] == '') {
+    redirect('public');
+}
 
 function redirect($url)
 {
