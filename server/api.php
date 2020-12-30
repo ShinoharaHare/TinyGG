619236403b2f75a4a7ffa31484a51b910127cf32
Hare <anna.ahiker1822@gmail.com>
2020-12-30 22:03:31 +0800

    feat: 實作刪除API

--- server/api.php
+++ server/api.php
@@ -14,7 +14,7 @@ $api->post('/shortened', function () {
 });
 
 $api->get('/shortened/:key', function ($key) {
-
+    
 });
 
 $api->put('/shortened/:key', function ($key) {
@@ -22,7 +22,24 @@ $api->put('/shortened/:key', function ($key) {
 });
 
 $api->delete('/shortened/:key', function ($key) {
+    DAO::query("SELECT original FROM Shortened WHERE `key` = '$key';");
+    $result = DAO::getResult();
 
+    if (empty($result)) {
+        http_response_code(404);
+        exit;
+    } else {
+        $briefID = $result[0]['original'];
+
+        DAO::query("DELETE FROM Shortened WHERE `key` = '$key';");
+        DAO::query("DELETE FROM Brief WHERE `ID` = '$briefID';");
+
+        http_response_code(204);
+    }
+});
+
+$api->get('/shortened/search', function () {
+    
 });
 
 // $api->post('/shortened/:key/', function ($key) {
